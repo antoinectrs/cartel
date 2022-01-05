730c91b71b236df7f28f2b3f821be4f9185b41fe
Antoine <antoinecontreras98@gmail.com>
2022-01-06 00:19:42 +0100

    push in array good

--- CARTEL_GIT/script/stateMachine.js
+++ CARTEL_GIT/script/stateMachine.js
@@ -1,6 +1,6 @@
 function changeStateMachine(){
   console.log(PARAMS.state.stateMachine);
-    if (PARAMS.state.stateMachine < 1) {
+    if (PARAMS.state.stateMachine <= 1) {
         PARAMS.state.stateMachine++;
       } else {
         PARAMS.state.stateMachine = 0;
@@ -8,14 +8,17 @@ function changeStateMachine(){
       return PARAMS.state.stateMachine;
 }
 function  sendLastLetterPosition(physicLetter,level){
-  console.log(PARAMS.separateWords[level]);
-  for (let index = 0; index < PARAMS.separateWords.length; index++) {
-    // console.log(physicLetter[index].length);
-    for (let i = 0; i <physicLetter[index].length; i++) {
-      PARAMS.positionWord.p0.LastPosition.push(physicLetter[index][i].body)
+  // console.log(physicLetter[level-1].length);
+  // for (let index = 0; index < 2; index++) {
+    // console.log(PARAMS.positionWord.p0.LastPosition);
+
+    PARAMS.positionWord.p0.LastPosition=[];
+    for (let i = 0; i <physicLetter[level-1].length; i++) {
+      PARAMS.positionWord.p0.LastPosition.push(physicLetter[level-1][i].body)
     }
+    console.log(PARAMS.positionWord.p0.LastPosition);
   }
-};
+// };
 function setUpWord(param) {
   PARAMS.separateWords=PARAMS.word[0].split(' ');
   for (let index = 0; index <PARAMS.separateWords.length; index++) {
